c90d54da828488c64a362a19eb043055cd8b8605
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2021-01-20 09:36:43 +0100

    Board display moved inside init

--- engine/view.py
+++ engine/view.py
@@ -3,7 +3,6 @@ from chessboard import display
 class View:
     def __init__(self, board):
         self.fen = board.fen()
-    def create_board(self):
         display.start(self.fen)
     def update_board(self, board):
         display.update(board.fen())
