25e9598f9e8b4bb18e2921546bc8c031965df19e
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2021-01-22 01:46:10 +0100

    Added move evaluation for AI

--- engine/move_evaluator.py
+++ engine/move_evaluator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import chess
 
 from engine.minimax import Minimax
 from engine.heuristics import Heuristics
@@ -16,7 +17,7 @@ class MoveEvaluator:
             current_move_evaluation = Minimax.minimax(board, heuristic_depth - 1, -
                         np.inf, np.inf, False, color)
 
-            MoveEvaluator.update_heuristics_dataset(board, current_move_evaluation, color)
+            MoveEvaluator.save_heuristics_into_dataset(board, current_move_evaluation, color)
             board.pop()
 
             if current_move_evaluation > max_eval:
@@ -27,7 +28,33 @@ class MoveEvaluator:
         return best_move
 
     @staticmethod
-    def update_heuristics_dataset(board, current_move_evaluation, color):
+    def predict_best_move(board, predictor, color):
+        best_move = None
+        max_eval = -np.inf
+
+        for move in board.legal_moves:
+            board.push(move)
+            
+            h1 = Heuristics.material_heuristic(board) if color == chess.WHITE else - Heuristics.material_heuristic(board)
+            h2 = Heuristics.piece_square_table_heuristic(board) if color == chess.WHITE else - Heuristics.piece_square_table_heuristic(board)
+            h3 = Heuristics.attack_heuristic(board) if color == chess.WHITE else - Heuristics.attack_heuristic(board)
+            features = predictor.scale([h1, h2, h3])
+
+            current_move_evaluation = predictor.predict(features)[0][0]
+
+            board.pop()
+
+            if current_move_evaluation > max_eval:
+                max_eval = current_move_evaluation
+                best_move = move
+
+        print(move)
+        return best_move
+
+
+
+    @staticmethod
+    def save_heuristics_into_dataset(board, current_move_evaluation, color):
         h1 = Heuristics.material_heuristic(board) if color == chess.WHITE else - Heuristics.material_heuristic(board)
         h2 = Heuristics.piece_square_table_heuristic(board) if color == chess.WHITE else - Heuristics.piece_square_table_heuristic(board)
         h3 = Heuristics.attack_heuristic(board) if color == chess.WHITE else - Heuristics.attack_heuristic(board)
@@ -39,4 +66,3 @@ class MoveEvaluator:
             current_move_evaluation
         ]
         Datasets.HEURISTICS_DATA.append(instance)
-
